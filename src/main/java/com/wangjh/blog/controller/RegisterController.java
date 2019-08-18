1f1ba4c5f0e9ef05b2f9a3e7dcae4830fdf966d7
wangjh626 <wangjh626@163.com>
2019-08-18 18:02:02 +0800

    修复注册时的 bug

--- src/main/java/com/wangjh/blog/controller/RegisterController.java
+++ src/main/java/com/wangjh/blog/controller/RegisterController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -39,29 +38,28 @@ public class RegisterController {
      */
     @PostMapping("/register")
     public ModelAndView registerUser(@RequestParam String phone, @RequestParam String username,
-                                     @RequestParam String password, RedirectAttributes redirectAttributes){
+                               @RequestParam String password){
         ModelAndView modelAndView = new ModelAndView("register");
         if (phone.length() != 11) {
-            redirectAttributes.addFlashAttribute("errorMessage", "请输入 11 位手机号");
+            modelAndView.addObject("errorMessage", "请输入 11 位手机号");
             return modelAndView;
         } else {
             // 检查该手机号是否已经注册
             User dbUser = userService.findByPhone(phone);
             if (dbUser != null) {
-                redirectAttributes.addFlashAttribute("errorMessage", "该手机号已注册");
+                modelAndView.addObject("errorMessage", "该手机号已注册");
                 return modelAndView;
-            } else {
-                redirectAttributes.addFlashAttribute("phone", phone);
             }
         }
         if (StringUtils.isEmpty(username)) {
-            redirectAttributes.addFlashAttribute("errorMessage", "请输入用户名");
+            modelAndView.addObject("phone", phone);
+            modelAndView.addObject("errorMessage", "请输入用户名");
             return modelAndView;
-        } else {
-            redirectAttributes.addFlashAttribute("username", username);
         }
         if (StringUtils.isEmpty(password)) {
-            redirectAttributes.addFlashAttribute("errorMessage", "请输入密码");
+            modelAndView.addObject("phone", phone);
+            modelAndView.addObject("username", username);
+            modelAndView.addObject("errorMessage", "请输入密码");
             return modelAndView;
         }
         // 使用 Shiro 自带的工具类生成 salt
